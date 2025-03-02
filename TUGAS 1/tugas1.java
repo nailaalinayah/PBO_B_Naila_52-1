47bdc644913dff59b2b41d99b827df3baf12cf8b
naila-alinayah <nailaalinayah1gmailcom@webmail.umm.ac.id>
2025-03-02 23:46:02 +0700

    Push modul 1

---
+++ TUGAS 1/tugas1.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class tugas1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String nimAkhir = "052";
+        String namaMahasiswa = "naila al inayah";
+        String nimMahasiswa = "202410370110052";
+
+        System.out.println("Pilih jenis login:");
+        System.out.println("1. Admin");
+        System.out.println("2. Mahasiswa");
+        System.out.print("Masukkan pilihan: ");
+        int pilihan = scanner.nextInt();
+        scanner.nextLine();
+
+
+
+        if (pilihan == 1) {
+            System.out.print("Masukkan username: ");
+            String username = scanner.nextLine();
+            System.out.print("Masukkan password: ");
+            String password = scanner.nextLine();
+
+            String validUsername = "Admin" + nimAkhir;
+            String validPassword = "Password" + nimAkhir;
+
+            if (username.equals(validUsername) && password.equals(validPassword)) {
+                System.out.println("Login Admin berhasil!");
+            } else {
+                System.out.println("Login gagal! Username atau password salah.");
+            }
+        } else if (pilihan == 2) {
+            System.out.print("Masukkan Nama: ");
+            String nama = scanner.nextLine();
+            System.out.print("Masukkan NIM: ");
+            String nim = scanner.nextLine();
+
+            if (nama.equals(namaMahasiswa) && nim.equals(nimMahasiswa)) {
+                System.out.println("Login Mahasiswa berhasil!");
+                System.out.println("Nama: " + nama);
+                System.out.println("NIM: " + nim);
+            } else {
+                System.out.println("Login gagal! Nama atau NIM salah.");
+            }
+        } else {
+            System.out.println("Pilihan tidak valid.");
+        }
+
+        scanner.close();
+    }
+}
